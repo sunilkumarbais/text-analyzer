f57c70ff001a2a54933bbb775ac16960323c564b
Sunil kumar <sunilkumarbais46@gmail.com>
2025-08-11 17:52:52 +0530

    Setup GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { TextAnalyzer } from './component/TextAnalyzer';
 import { Error } from "./component/Error";
 import './App.css';
@@ -6,10 +6,10 @@ import './App.css';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TextAnalyzer/>}/>
-      <Route path="*" element={<Error/>}/>
+      <Route path="/" element={<TextAnalyzer />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import {BrowserRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
-
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
+  <BrowserRouter basename="/Text-Analyzer">
+    <App />
+  </BrowserRouter>
 );
-
